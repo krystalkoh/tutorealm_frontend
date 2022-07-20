b7c916de43c96fb4f0349a9800cd3df57e93b7be
Krystal <krystalkoh@gmail.com>
2022-07-21 00:03:37 +0800

    landing done

--- react_app/src/components/LandingPage.js
+++ react_app/src/components/LandingPage.js
@@ -14,21 +14,35 @@ const LandingPage = () => {
   };
   return (
     <>
+      <button
+        onClick={handleLoginClick}
+        id="button"
+        class="rounded px-4 py-2 mt-4 ml-4 bg-violet-300 text-xl"
+      >
+        Login
+      </button>
+      <button
+        id="button"
+        class="rounded px-4 py-2 mt-4 ml-4 bg-violet-300 text-xl"
+        onClick={handleRegisterClick}
+      >
+        Register
+      </button>
       <header class="flex justify-between items-center mt-28">
         <div class="pl-40 w-3/5 ">
           <div>Introducing</div>
           <div id="title" class="text-8xl">
             TUTOREALM
           </div>
-          <div class="text-red-700 mt-4"> Short description here</div>
+          <p class="text-red-700 mt-4"> Short description here</p>
         </div>
+
         <div class="w-2/5 mr-40">
           <img src="https://i.imgur.com/G7Npd0b.jpg" alt="Login Picture" />
         </div>
+
+        <br />
       </header>
-      <br />
-      <button onClick={handleLoginClick}>Login</button>
-      <button onClick={handleRegisterClick}>Register</button>
     </>
   );
 };
