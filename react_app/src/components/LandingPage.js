c3ed70b1122c18154ff2c62564716d227d05db23
Krystal <krystalkoh@gmail.com>
2022-07-21 00:10:30 +0800

    landing page css completed

--- react_app/src/components/LandingPage.js
+++ react_app/src/components/LandingPage.js
@@ -17,27 +17,37 @@ const LandingPage = () => {
       <button
         onClick={handleLoginClick}
         id="button"
-        class="rounded px-4 py-2 mt-4 ml-4 bg-violet-300 text-xl"
+        class="rounded px-5 py-2 mt-4 ml-4 bg-slate-300 text-xl"
       >
         Login
       </button>
       <button
         id="button"
-        class="rounded px-4 py-2 mt-4 ml-4 bg-violet-300 text-xl"
+        class="rounded px-4 py-2 mt-4 ml-4 bg-slate-300 text-xl"
         onClick={handleRegisterClick}
       >
         Register
       </button>
       <header class="flex justify-between items-center mt-28">
         <div class="pl-40 w-3/5 ">
-          <div>Introducing</div>
+          <div id="button" class="text-xl">
+            {" "}
+            Introducing
+          </div>
           <div id="title" class="text-8xl">
             TUTOREALM
           </div>
-          <p class="text-red-700 mt-4"> Short description here</p>
+          <p class="text-red-700 mt-4 text-lg mr-10" id="button">
+            {" "}
+            Tutorealm: an app designed with freelance tutors and vendors in
+            mind. We bring the convenience of a common platform where tutors and
+            vendors can communicate to fulfill their needs. Be it looking for
+            tutees or looking for tutors, we go beyond to connect your worlds
+            together in our realm.
+          </p>
         </div>
 
-        <div class="w-2/5 mr-40">
+        <div class="w-2/5 mr-40 ml-10">
           <img src="https://i.imgur.com/G7Npd0b.jpg" alt="Login Picture" />
         </div>
 
